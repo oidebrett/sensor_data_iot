3af5de55a7ea193d633c58ac560a591621312f6f
Oide Brett <32061073+oidebrett@users.noreply.github.com>
2021-11-29 16:55:42 +0000

    Update pythonToHttpPost.py

--- pythonToHttpPost.py
+++ pythonToHttpPost.py
@@ -6,8 +6,8 @@ baseurl = 'https://sensor-data-example.glitch.me/addData'
 
 while (True):
     randomFloat = random.random()
-    temp = 14.00 + randomFloat
-    myobj = {'field': "temp", 'value': temp}
+    tempValue = 14.00 + randomFloat
+    myobj = {'field': "temp", 'value': tempValue}
     x = requests.post(baseurl, data = myobj)
     print(x.text)
     time.sleep( 1 )
